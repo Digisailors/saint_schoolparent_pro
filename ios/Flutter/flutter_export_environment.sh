6e065e018f65d81cb84959d3803db73cb1c46b6f
ilaiya-raja <pitchuclassy@gmail.com>
2023-02-27 20:56:29 +0530

    .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Work\Saint school\Parent\saint_schoolparent_pro"
+export "FLUTTER_ROOT=/Users/raja/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/raja/Desktop/saint_schoolparent_pro"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=10"
+export "FLUTTER_BUILD_NAME=1.0.14"
+export "FLUTTER_BUILD_NUMBER=14"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
